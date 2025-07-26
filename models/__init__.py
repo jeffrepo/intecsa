13c164f6bfb5227ac980d804a13a635f5d4c3110
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2025-07-26 09:54:26 -0600

    Ya no se utiliza pos

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,6 @@ from . import sale_order
 from . import stock_picking
 from . import res_partner_bank
 from . import account_journal
-from . import pos_session
+#from . import pos_session
 #from . import account_aged_partner_balance
 from . import account_report
