5cf0b5d1056165733d60329f118a0ec000447059
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2025-08-30 11:22:08 -0600

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -8,4 +8,4 @@ from . import res_partner_bank
 from . import account_journal
 #from . import pos_session
 #from . import account_aged_partner_balance
-from . import account_report
+#from . import account_report
